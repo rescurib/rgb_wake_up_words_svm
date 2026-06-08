bfa1e3244df99850e79a4e75359dc87f731d4db8
Rodolfo Escobar <r.escurib@gmail.com>
2026-06-07 20:47:10 -0600

    add rbf kernel svm.

--- Src/clasificador_svm.c
+++ Src/clasificador_svm.c
@@ -1,18 +1,18 @@
 /**
  * @file clasificador_svm.c
- * @brief Implementation of functions to initialize and predict using polynomial SVMs.
+ * @brief Implementation of functions to initialize and predict using RBF SVMs.
  */
 
 #include "clasificador_svm.h"
 #include "svm_params.h"
 
 // Contexts for each SVM classifier "One vs Rest"
-static arm_svm_polynomial_instance_f32 svm_blue_ctx;
-static arm_svm_polynomial_instance_f32 svm_green_ctx;
-static arm_svm_polynomial_instance_f32 svm_red_ctx;
+static arm_svm_rbf_instance_f32 svm_blue_ctx;
+static arm_svm_rbf_instance_f32 svm_green_ctx;
+static arm_svm_rbf_instance_f32 svm_red_ctx;
 
 // Array to easily iterate SVM contexts during prediction
-static arm_svm_polynomial_instance_f32* svm_ctx_array[NB_CLASSIFIERS];
+static arm_svm_rbf_instance_f32* svm_ctx_array[NB_CLASSIFIERS];
 
 /**
  * @brief Initializes each classifier indicating its precomputed support vectors and dual variables.
@@ -20,38 +20,32 @@ static arm_svm_polynomial_instance_f32* svm_ctx_array[NB_CLASSIFIERS];
 void clasificador_svm_init(void)
 {
     // blue vs rest
-    arm_svm_polynomial_init_f32(&svm_blue_ctx,
-                                NB_SV_BLUE_VS_REST,
-                                VECTOR_DIMENSION,
-                                INTERCEPT_BLUE_VS_REST,
-                                dualCoefficients_blue_vs_rest,
-                                supportVectors_blue_vs_rest,
-                                classes_blue_vs_rest,
-                                DEGREE_BLUE_VS_REST,
-                                COEF0_BLUE_VS_REST,
-                                GAMMA_BLUE_VS_REST);
+    arm_svm_rbf_init_f32(&svm_blue_ctx,
+                         NB_SV_BLUE_VS_REST,
+                         VECTOR_DIMENSION,
+                         INTERCEPT_BLUE_VS_REST,
+                         dualCoefficients_blue_vs_rest,
+                         supportVectors_blue_vs_rest,
+                         classes_blue_vs_rest,
+                         GAMMA_BLUE_VS_REST);
     // green vs rest
-    arm_svm_polynomial_init_f32(&svm_green_ctx,
-                                NB_SV_GREEN_VS_REST,
-                                VECTOR_DIMENSION,
-                                INTERCEPT_GREEN_VS_REST,
-                                dualCoefficients_green_vs_rest,
-                                supportVectors_green_vs_rest,
-                                classes_green_vs_rest,
-                                DEGREE_GREEN_VS_REST,
-                                COEF0_GREEN_VS_REST,
-                                GAMMA_GREEN_VS_REST);    
+    arm_svm_rbf_init_f32(&svm_green_ctx,
+                         NB_SV_GREEN_VS_REST,
+                         VECTOR_DIMENSION,
+                         INTERCEPT_GREEN_VS_REST,
+                         dualCoefficients_green_vs_rest,
+                         supportVectors_green_vs_rest,
+                         classes_green_vs_rest,
+                         GAMMA_GREEN_VS_REST);    
     // red vs rest
-    arm_svm_polynomial_init_f32(&svm_red_ctx,
-                                NB_SV_RED_VS_REST,
-                                VECTOR_DIMENSION,
-                                INTERCEPT_RED_VS_REST,
-                                dualCoefficients_red_vs_rest,
-                                supportVectors_red_vs_rest,
-                                classes_red_vs_rest,
-                                DEGREE_RED_VS_REST,
-                                COEF0_RED_VS_REST,
-                                GAMMA_RED_VS_REST);
+    arm_svm_rbf_init_f32(&svm_red_ctx,
+                         NB_SV_RED_VS_REST,
+                         VECTOR_DIMENSION,
+                         INTERCEPT_RED_VS_REST,
+                         dualCoefficients_red_vs_rest,
+                         supportVectors_red_vs_rest,
+                         classes_red_vs_rest,
+                         GAMMA_RED_VS_REST);
                         
     svm_ctx_array[0] = &svm_blue_ctx;
     svm_ctx_array[1] = &svm_green_ctx;
@@ -71,7 +65,7 @@ void clasificador_svm_predict(float32_t *feature_vector, uint32_t feature_vector
     *label = -1;
     for(int i = 0; i < NB_CLASSIFIERS; i++)
     {
-        arm_svm_polynomial_predict_f32(svm_ctx_array[i], feature_vector, label);
+        arm_svm_rbf_predict_f32(svm_ctx_array[i], feature_vector, label);
         if (*label == svm_ctx_array[i]->classes[1]) // If inference indicates recognized class
         {
             break; // Exit the iteration early
